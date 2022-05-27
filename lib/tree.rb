857801f22de34f172da333777009cde24ade3028
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-05-26 21:51:00 -0400

    fixed issue of infinitely alternating moves by keeping track of every move and preventing repeated moves to already visited locations

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,22 +6,29 @@ class Tree
     @root = Node.new(start)
     @finish = finish
     @board = Array.new(8) {Array.new(8) {'_'}}
+    @visited = []
     build_tree
   end
-  #recursively build a tree where the children of every node is its potential moves
+  #recursively build a tree where the children of every node is its potential moves, stopping when the end location is reached
   def build_tree(node = @root)
   
-    moves = get_moves(node.coord)
+    moves = get_moves(node.coord) - @visited
+    p moves
     if moves.include?(@finish)
+      p "something"
       node.children << Node.new(@finish)
       return
      else
-      moves.each { |move| node.children << Node.new(move) }
+      moves.each  do |move| 
+        node.children << Node.new(move) 
+        @visited << move
+      end
       node.children.each { |child| build_tree(child) } 
     end
   end
   #get all potential moves of a given coordinate, excluding moves that would result in going off the board
   def get_moves(coord)
+    
     moves = []
     possible_moves = [1, -1, 2, -2].permutation(2).to_a.reject { |y, x| y.abs == x.abs}
     possible_moves.each do |y, x|
